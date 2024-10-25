31c2e424bebebbb225bff08605e2e7a388aec8ac
Dika <masdikaaliman@gmail.com>
2024-10-25 14:35:24 +0700

    Update Code

---
+++ compile_tes.sh
@@ -0,0 +1,2 @@
+g++ -c -I$HALCONROOT/include -I$HALCONROOT/include/halconcpp -I$HALCONROOT/lib/x64-linux PlateDetectionDeepOCR.cpp -o plate_detect.o
+
---
+++ linking_tes.sh
@@ -0,0 +1,2 @@
+g++ plate_detect.o -L$HALCONROOT/lib/x64-linux -lhalconcpp -lhalcon -o plate_detect
+
